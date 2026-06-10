bef33b504cdac53096698f8fed2f5f8db3ef9103
hsm2456 <hsm030305@gmail.com>
2026-06-10 21:43:30 +0900

    Delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-const { GoogleGenerativeAI } = require('@google/generative-ai');
-
-const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY || 'AIzaSyBZs0ZIfxAsTDLZZ2d2syC6EwiaNwuNdBE');
-
-async function run() {
-  const models = await genAI.getModels();
-  console.log(models);
-}
-run();
